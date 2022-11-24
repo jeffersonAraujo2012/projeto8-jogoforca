fe8f3d1eb32fde3bd347b3be2801975e8e85d70d
Jefferson Araújo <jeffersonc201212@gmail.com>
2022-11-24 19:50:06 -0300

    Style: alterações no componente PalavraJogo

--- src/components/Jogo/PalavraJogo.jsx
+++ src/components/Jogo/PalavraJogo.jsx
@@ -1,5 +1,22 @@
-function PalavraJogo() {
-  return <></>;
+import styled from "styled-components";
+
+function PalavraJogo({className}) {
+  return (
+    <StyledPalavraJogo className={className} >
+      _ _ _ _ _ _
+    </StyledPalavraJogo>
+  );
 }
+
+const StyledPalavraJogo = styled.div`
+  font-family: "Noto Sans";
+  font-weight: 700;
+  font-size: 50px;
+  line-height: 68px;
+
+  display: flex;
+  align-items: center;
+  text-align: center;
+`;
 
 export default PalavraJogo;
